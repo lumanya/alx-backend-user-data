6cb29d2718d6d3e56d603ea55449c222ca52f589
lumanya <shaibulumanya@gmail.com>
2024-11-07 16:41:14 +0300

    check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,6 +4,12 @@ Encrypts the password using bcrypt
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """Encrypts the password using bcrypt"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the password is valid"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
